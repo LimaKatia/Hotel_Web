508ecb3d1f9ef4ec56526f741d51dd3657e503eb
Vasquez8 <dv113409@gmail.com>
2024-02-24 16:30:31 -0600

    Creación de la clase Entidad EstadoEN

---
+++ CapaEN/EstadoEN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEN
+{
+    public class EstadoEN
+    {
+        [Key]
+        public byte Id { get; set; }
+
+        [Display(Name = "Tipo de estado")]
+        [Required(ErrorMessage ="El tipo de estado es requeido")]
+        [StringLength(50,ErrorMessage ="Máximo 50 Carcteres")]
+        public string Nombre { get; set; } = string.Empty;
+    }
+}
